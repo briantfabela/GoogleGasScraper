291e2af5ec3e74cd3dce491ad9331ab58887cdcc
Briant Fabela <briantfabela@umail.ucsb.edu>
2020-01-16 15:58:12 -0700

    helpfuncs.py working
    added drivers, set up helpfuncs thus far, updated readme, TimeoutExceptions appearing towards the end of the list (the last 5 entries or so); will test increasing explicit wait time.

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # GoogleGasScraper
 A web scraping algorithm using Selenium on python 3.7.1 that visits a list of known gas stations with listed gas prices, scrapes them, and writes them to csv file.
+
+A python web crawler can be deployed on a headless browser to identify gas stations from the google maps search sidebar (scrollbox iterating through all search results) that have price information available and mine the gas stations that will be used to get info via the scraper.
--- helpfuncs.py
+++ helpfuncs.py
@@ -7,9 +7,12 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
-from time import sleep
-from os import path
+# Selenium exception handling
+from selenium.common.exceptions import NoSuchElementException
+from selenium.common.exceptions import TimeoutException
 
+from datetime import datetime
+from time import ctime
 
 def read_addresses(txt_file_path):
     '''Returns a text file of addresses as a python list'''
@@ -30,6 +33,17 @@ class GeoInfo:
         self.lat = float(x)
         self.long = float(y)
 
+class GasPrices:
+    '''Stores fuel prices as scraped from google maps'''
+
+    def __init__(self, diesel, reg, mid, premium):
+        self.diesel = diesel
+        self.regular = reg
+        self.midgrade = mid
+        self.premium = premium
+        # timestamp will be in local time (MST for me)
+        self.timestamp = ctime(datetime.now().timestamp())
+
 class GasPriceChecker:
     '''Uses the selenium driver to visit a list of gas station addresses'''
 
@@ -38,6 +52,13 @@ class GasPriceChecker:
         self.xpaths = xpaths # xpath dictionary
         self.locations = read_addresses(addresses_txt_file_path)
         self.driver_fp = r'chromedriver_win32/chromedriver_v79.exe'
+
+    def parse_prices(self, string):
+        '''Parses thru prices string and returns a GasPrices instance'''
+
+        return GasPrices(
+            *[i.strip('$') for i in string.split() if '$' in i or '-' in i]
+        )
     
     def check(self, max_window=True, dims=(1080,800)):
         '''Opens the webridriver using Selenium.
@@ -53,45 +74,66 @@ class GasPriceChecker:
             self.driver.maximize_window()
         else:
             self.driver.set_window_size(*dims)
+
         self.driver.get(self.url)
 
         for loc in self.locations:
-            # lets shorten self.driver.find_element_by_xpath
+            # shorten self.driver.find_element_by_xpath
             find_by_xpath = self.driver.find_element_by_xpath
             field = find_by_xpath(self.xpaths['searchField'])
             field.send_keys(loc) # type in location
 
             field_button = find_by_xpath(self.xpaths['searchButton'])
             field_button.click() # click search button
+    
+            try:
+                # wait for element to be visible
+                wait = WebDriverWait(self.driver, 5).until(
+                    EC.visibility_of_element_located(
+                        (By.CLASS_NAME, 'section-gas-prices-container')
+                    )
+                )
 
-            # Lets catch search results screen if it appears
-            if len(self.driver.current_url) < 80:
-                print('Possible Search Results Screen')
-                sleep(3) # allow search results to load
-                # click on first result
-                try:
-                    # usually this is the right xpath
-                    find_by_xpath(self.xpaths['searchResult1']).click()
-                except:
-                    # fallback
-                    find_by_xpath(self.xpaths['searchResult2']).click()
+                # get prices
+                raw_prices = self.driver.find_element_by_class_name(
+                'section-gas-prices-container').text
 
-            '''
-            wait = WebDriverWait(self.driver, 10).until( # wait until the streetview loads
-                EC.visibility_of_element_located((By.XPATH, self.xpaths['titleCard']))
-            )
+                # parse and assign
+                self.prices = self.parse_prices(raw_prices)
+                # get coordinates
+                self.geo = get_latlong(self.driver.current_url)
 
-            # get lat and long; print to console
-            latlong = get_latlong(self.driver.current_url)
-            print("lat: {} long: {}".format(latlong.lat, latlong.long))'''
+                # print all the info
+                print(loc, 
+                "Coords: "+str(self.geo.lat)+", "+str(self.geo.long),
+                "Disl: "+self.prices.diesel, 
+                "Regl: "+self.prices.regular,
+                "Midg: "+self.prices.midgrade,
+                "Perm: "+self.prices.premium, sep='\n')
+    
+            except TimeoutException:
+                print("TimeoutException")
+                print(loc, "passed.")
+                pass
 
-            back_button = find_by_xpath(self.xpaths['backButton'])
-            back_button.click() # click back button
-
-            wait = WebDriverWait(self.driver, 10).until( # searchField needs some time to load
-                EC.visibility_of_element_located((By.XPATH, self.xpaths['searchField']))
-            )
+            except NoSuchElementException:
+                print("NoSuchElementException")
+                print(loc, "passed.")
+                pass
 
             field.clear()
 
         print("Tour has ended.")
+
+# test code
+def main():
+    xpaths = dict(
+        searchField = '//*[@id="searchboxinput"]',
+        searchButton = '//*[@id="searchbox-searchbutton"]'
+    )
+    
+    x = GasPriceChecker('https://www.google.com/maps', xpaths, 'gas_stations.txt')
+    x.check()
+
+if __name__ == "__main__":
+    main()
